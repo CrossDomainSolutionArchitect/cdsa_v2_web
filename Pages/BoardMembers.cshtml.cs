1c9a1d6fa33e91971edeb012a92b127234859e20
CrossDomainSolutionArchitect <vernard.ngomane@vcdsa.co.za>
2025-09-30 05:29:29 +0200

    website v1

---
+++ Pages/BoardMembers.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace cdsa.solutions.Pages
+{
+    public class BoardMembersModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
